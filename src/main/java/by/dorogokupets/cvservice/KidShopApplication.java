cfdce02accb1722dc31d55a490512eb859c94f7e
Darahakupets <ilyadorogokupets@yahoo.com>
2023-12-13 16:44:02 +0300

    13.12.2023

---
+++ src/main/java/by/dorogokupets/cvservice/KidShopApplication.java
@@ -0,0 +1,13 @@
+package by.dorogokupets.cvservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KidShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KidShopApplication.class, args);
+	}
+
+}
